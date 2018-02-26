69bc236fb66530e2586f74abb68d47ec1b123bc6
Albert Huynh <ahuynh@fanatics.com>
2018-02-25 21:48:04 -0800

    Testing more configs for heroku deploy

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var server = app.listen(app.get('port'), function () {
     console.log('listening on port ', server.address().port);
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('port', process.env.PORT || 9000);
+app.use(express.static(path.join(__dirname, 'app/index.js')));
+app.set('port', 9000);
+// App.set('port', process.env.PORT || 9000);
 
