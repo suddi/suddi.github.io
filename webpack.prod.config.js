49df64981e2553c5601496ddf7b734a7493137e7
Albert Huynh <alberthuynh91@gmail.com>
2018-03-06 21:00:18 -0800

    Update package json

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'public'),
         filename: 'bundle.js',
-        publicPath: '/public/'
+        publicPath: 'public/'
     },
 
     plugins: [
